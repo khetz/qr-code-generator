df0ac163371082bb67911570245c34f454bc56d6
khetz <vukheta99@gmail.com>
2026-05-14 21:58:50 +0200

    Addedd qr input section

---
+++ src/app/features/qr-generation/components/qr-input-section/qr-input-section.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-qr-input-section',
+  imports: [MatIconModule],
+  templateUrl: './qr-input-section.html',
+  styleUrl: './qr-input-section.css',
+})
+export class QrInputSection {}
--- src/app/layout/home/home.ts
+++ src/app/layout/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { QrInputTypeSection } from "../../features/qr-generation/components/qr-input-type-section/qr-input-type-section";
+import { QrInputSection } from "../../features/qr-generation/components/qr-input-section/qr-input-section";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, QrInputTypeSection],
+  imports: [Header, QrInputTypeSection, QrInputSection],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
