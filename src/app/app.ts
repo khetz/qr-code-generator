7487711cd502d10e455a9b684495c8be70e3b8c5
khetz <vukheta99@gmail.com>
2026-05-19 22:10:05 +0200

    Removed unused import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { QRCodeComponent } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, QRCodeComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
