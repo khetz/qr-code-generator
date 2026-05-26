36cf288fb81c9c5f8521f8d885ffc76a203f4193
khetz <vukheta99@gmail.com>
2026-05-26 21:08:03 +0200

    Started tablet media queries for entire site

--- src/app/features/qr-generation/components/qr-code-preview/qr-code-preview.ts
+++ src/app/features/qr-generation/components/qr-code-preview/qr-code-preview.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { QRCodeComponent } from 'angularx-qrcode';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-qr-code-preview',
@@ -7,6 +8,26 @@ import { QRCodeComponent } from 'angularx-qrcode';
   templateUrl: './qr-code-preview.html',
   styleUrl: './qr-code-preview.css',
 })
-export class QrCodePreview {
+export class QrCodePreview implements OnInit {
   @Input() qrData = '';
+  qrCodeWidth = 150;
+
+  private breakpointObserver = inject(BreakpointObserver);
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe([
+      Breakpoints.Handset,
+      Breakpoints.Tablet,
+      Breakpoints.Web
+    ]).subscribe(result => {
+      if (result.breakpoints[Breakpoints.Handset]) {
+        this.qrCodeWidth = 150;
+      } else if (result.breakpoints[Breakpoints.Tablet]) {
+        this.qrCodeWidth = 200;
+      } else {
+        this.qrCodeWidth = 300;
+      }
+      
+    })
+  }
 }
