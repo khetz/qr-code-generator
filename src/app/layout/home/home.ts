99ff2b1642327595390f0969c68415495b778d34
khetz <vukheta99@gmail.com>
2026-06-01 20:02:08 +0200

    Removed unused qr input type component

--- src/app/layout/home/home.ts
+++ src/app/layout/home/home.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { Header } from '../header/header';
-import { QrInputTypeSection } from "../../features/qr-generation/components/qr-input-type-section/qr-input-type-section";
-import { QrInputSection } from "../../features/qr-generation/components/qr-input-section/qr-input-section";
 import { QrCodePreview } from "../../features/qr-generation/components/qr-code-preview/qr-code-preview";
+import { QrInputSection } from "../../features/qr-generation/components/qr-input-section/qr-input-section";
+import { Header } from '../header/header';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, QrInputTypeSection, QrInputSection, QrCodePreview],
+  imports: [Header, QrInputSection, QrCodePreview],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
