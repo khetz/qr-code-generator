70a0800f16c3f0815991db21a3582dc164adc630
khetz <vukheta99@gmail.com>
2026-06-06 18:36:12 +0200

    Added history clearing and navigation to history page in recent history component

--- src/app/shared/components/recent-history.component/recent-history.component.ts
+++ src/app/shared/components/recent-history.component/recent-history.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { HistoryService } from '../../../core/services/history.service';
 import { HistoryEntryComponent } from '../history-entry.component/history-entry.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recent-history',
@@ -9,12 +10,22 @@ import { HistoryEntryComponent } from '../history-entry.component/history-entry.
   styleUrl: './recent-history.component.css',
 })
 export class RecentHistoryComponent implements OnInit {
-  historyService = inject(HistoryService);
-  readonly historyList = this.historyService.qrHistory;
-
   readonly recentHistoryEntryLimit = 5;
+
+  private readonly historyService = inject(HistoryService);
+
+  readonly historyList = this.historyService.qrHistory;
+  private readonly router = inject(Router);
 
   ngOnInit(): void {
     this.historyService.getHistory();
+  }
+
+  clearAllHistory() {
+    this.historyService.clearAllHistoryEntries();
+  }
+
+  goToViewAllPage() {
+    this.router.navigateByUrl('view-all-history');
   }
 }
