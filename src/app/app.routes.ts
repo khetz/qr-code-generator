d92b66ac2d7832acb178b9b897c967976f3b917f
khetz <vukheta99@gmail.com>
2026-06-06 18:34:58 +0200

    Added route to history page and set initial page layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './layout/home/home';
+import { HistoryPageComponent } from './features/history/pages/history-page.component/history-page.component';
 
 export const routes: Routes = [
-    { path: '', component: Home }
+    { path: '', component: Home },
+    { path: 'view-all-history', component: HistoryPageComponent }
 ];
--- src/app/features/history/pages/history-page.component/history-page.component.ts
+++ src/app/features/history/pages/history-page.component/history-page.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Header } from '../../../../layout/header/header';
+import { Router } from '@angular/router';
+import { HistoryService } from '../../../../core/services/history.service';
+import { HistoryEntryComponent } from '../../../../shared/components/history-entry.component/history-entry.component';
+import { History } from '../../../../shared/models/history.model';
 
 @Component({
   selector: 'app-history-page',
-  imports: [],
+  imports: [Header, HistoryEntryComponent],
   templateUrl: './history-page.component.html',
   styleUrl: './history-page.component.css',
 })
-export class HistoryPageComponent {}
+export class HistoryPageComponent implements OnInit {
+  private readonly router = inject(Router);
+  private readonly historyService = inject(HistoryService);
+  readonly historyList = this.historyService.qrHistory;
+
+  ngOnInit(): void {
+    this.historyService.getHistory();
+  }
+
+  goBackToHome() {
+    this.router.navigateByUrl('');
+  }
+
+  clearAllHistory() {
+    this.historyService.clearAllHistoryEntries();
+  }
+
+  get reversedList(): History[] {
+    return [...this.historyList()].reverse();
+  }
+}
