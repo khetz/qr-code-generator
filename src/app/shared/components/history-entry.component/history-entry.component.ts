dbbf1c67761c39fe818e76256002ef971ac8b16f
khetz <vukheta99@gmail.com>
2026-06-06 13:34:23 +0200

    Added recent history and history entry components

---
+++ src/app/shared/components/history-entry.component/history-entry.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-history-entry',
+  imports: [],
+  templateUrl: './history-entry.component.html',
+  styleUrl: './history-entry.component.css',
+})
+export class HistoryEntryComponent {}
---
+++ src/app/shared/components/recent-history.component/recent-history.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-history',
+  imports: [],
+  templateUrl: './recent-history.component.html',
+  styleUrl: './recent-history.component.css',
+})
+export class RecentHistoryComponent {}
