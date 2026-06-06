b60d7da26d488dfb090a9a5a0fd630149acdbfc2
khetz <vukheta99@gmail.com>
2026-06-06 12:41:09 +0200

    Implemented whatsapp and email sharing

--- src/app/shared/dropdowns/share-menu.component/share-menu.component.ts
+++ src/app/shared/dropdowns/share-menu.component/share-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
+import { ToastService } from '../../../core/services/toast.service';
 
 @Component({
   selector: 'app-share-menu',
@@ -6,4 +7,39 @@ import { Component } from '@angular/core';
   templateUrl: './share-menu.component.html',
   styleUrl: './share-menu.component.css',
 })
-export class ShareMenuComponent {}
+export class ShareMenuComponent {
+
+  qrlinkToShare = input.required<string>();
+  closeMenu = output<void>();
+
+  toastService = inject(ToastService);
+
+  encodeUrl() {
+    return encodeURIComponent(this.qrlinkToShare());
+  }
+
+  whatsappShare() {
+    if (this.qrlinkToShare() == '') {
+      this.toastService.error('Cannot share empty qr url');
+      return;
+    }
+
+    this.closeMenu.emit();
+    const encodedUrl = this.encodeUrl();
+    window.open(`https://wa.me/?text=${encodedUrl}`)
+  }
+
+  emailShare() {
+    if (this.qrlinkToShare() == '') {
+      this.toastService.error('Cannot share empty qr url');
+      return;
+    }
+
+    this.closeMenu.emit();
+    const encodedSubject = encodeURIComponent('Check this link out');
+    let encodedBody = this.encodeUrl();
+
+    const emailLink = `mailto:?subject=${encodedSubject}&body=${encodedBody}`;
+    window.location.href = emailLink;
+  }
+}
