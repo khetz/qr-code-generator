f7f07eb3a2960bb21154b288e19bfa0ea354c9a1
khetz <vukheta99@gmail.com>
2026-06-06 13:35:53 +0200

    Added history page component

---
+++ src/app/features/history/pages/history-page.component/history-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-history-page',
+  imports: [],
+  templateUrl: './history-page.component.html',
+  styleUrl: './history-page.component.css',
+})
+export class HistoryPageComponent {}
