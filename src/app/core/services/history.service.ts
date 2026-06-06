d8db076bc5e264c55ab096ab671ec337b7843f25
khetz <vukheta99@gmail.com>
2026-06-06 13:27:42 +0200

    Added history model and implemented first version of history service

---
+++ src/app/core/services/history.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HistoryService {
+  private qrGenerationHistory = signal<History[]>([]);
+  readonly qrHistory = this.qrGenerationHistory.asReadonly();
+
+  private readonly localStorageHistoryName = "history";
+
+  getHistory() {
+    let history = localStorage.getItem(this.localStorageHistoryName);
+
+    if (!history) return;
+
+    this.qrGenerationHistory.set(JSON.parse(history) as History[]);
+  }
+
+  addHistoryEntry(historyEntry: History) {
+    this.qrGenerationHistory.set([...this.qrGenerationHistory(), historyEntry]);
+    localStorage.setItem(this.localStorageHistoryName, JSON.stringify(this.qrGenerationHistory()));
+  }
+
+  clearHistoryEntry() {
+    this.qrGenerationHistory.set([]);
+    localStorage.setItem(this.localStorageHistoryName, JSON.stringify(this.qrGenerationHistory()));
+  }
+}
