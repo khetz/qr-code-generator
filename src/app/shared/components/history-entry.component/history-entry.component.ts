1bd65eabb755774494c5689f89b3320768e38cea
khetz <vukheta99@gmail.com>
2026-06-06 15:17:28 +0200

    Added history entry url formatting

--- src/app/shared/components/history-entry.component/history-entry.component.ts
+++ src/app/shared/components/history-entry.component/history-entry.component.ts
@@ -1,5 +1,6 @@
 import { Component, input } from '@angular/core';
 import { History } from '../../models/history.model';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-history-entry',
@@ -9,4 +10,14 @@ import { History } from '../../models/history.model';
 })
 export class HistoryEntryComponent {
   readonly history = input.required<History>();
+
+  private readonly urlLengthLimit = 10;
+
+  formatUrl(url: SafeUrl): string {
+    const stringUrl = url.toString();
+
+    return stringUrl.length > this.urlLengthLimit 
+    ? url.toString().substring(0, this.urlLengthLimit) + "..."
+    : stringUrl;
+  }
 }
